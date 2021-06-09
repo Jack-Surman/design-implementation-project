23ef48dbd648a4e305f7a90421b1f43fba301a2b
leszekmyrkwa <myrkwa97@gmail.com>
2021-06-09 12:00:11 +0100

    Header navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.scss";
+const hamburgerMenuIcon = document.getElementById('header-icon');
+const headerMenu = document.getElementById('header-nav');
+const contactButton = document.getElementById('header-button');
+hamburgerMenuIcon.addEventListener('click', () => {
+    headerMenu.classList.toggle('hamburgerToggle');
+})
