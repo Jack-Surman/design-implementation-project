99046d39465c70673c1ea75ac7383d8592697576
leszekmyrkwa <myrkwa97@gmail.com>
2021-06-14 08:23:46 +0100

    Gallery section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./style.scss";
 import HamburgerMenu from "./js/hamburgerMenu";
 import AccordionClick from "./js/accordionClick";
+import GalleryPages from "./js/gallery-pages";
 new HamburgerMenu();
 new AccordionClick();
+new GalleryPages();
---
+++ src/js/gallery-pages.js
@@ -0,0 +1,37 @@
+export default class GalleryPages {
+    constructor() {
+        this.changePage();
+    }
+    changePage() {
+        const galleryNav = document.getElementById('gallery-nav').querySelectorAll('button');
+        galleryNav.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                button.classList.remove('text-dark-bold');
+                galleryNav.forEach((button) => {
+                   button.classList.remove('text-dark-bold')
+                    button.classList.add('text-gray');
+                });
+                button.classList.add('text-dark-bold');
+                button.classList.remove('text-gray');
+                const buttonNum = Number(e.target.dataset.num);
+                this.changeImage(buttonNum);
+            });
+        });
+    }
+    changeImage(buttonNum) {
+        const imageElements = document.getElementById('gallery-img').querySelectorAll('img');
+        imageElements.forEach((image) => {
+            let imgNum = Number(image.dataset.num);
+            if (buttonNum === 2) {
+                imgNum = imgNum + 3;
+            } else if (buttonNum === 3) {
+                imgNum = imgNum + 6;
+            } else if (buttonNum === 4) {
+                imgNum = imgNum + 9;
+            } else if (buttonNum === 5) {
+                imgNum = imgNum + 12;
+            }
+            image.setAttribute('src', `/img/gallery-img-${imgNum}.PNG`);
+        });
+    }
+}
