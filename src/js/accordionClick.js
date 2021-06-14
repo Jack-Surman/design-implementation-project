7a2c0d04264f3e3215cc8273733be2e7f8bdf0f1
John Theis <john.theis@room58.com>
2021-06-14 09:45:30 +0100

    Feedback

--- src/js/accordionClick.js
+++ src/js/accordionClick.js
@@ -6,6 +6,10 @@ export default class AccordionClick {
         const accordionElements = document.querySelectorAll('.accordion-section-item');
         accordionElements.forEach((element) => {
             element.addEventListener('click', (e) => {
+                /** TODO: Good implementation. One little performance enhancement:
+                 * Rather than going through every element in the item and hiding it.
+                 * Just add one class to the item, and target the elements you want to hide in CSS
+                 */
                 const chevronEl = element.querySelector('i');
                 const accordionContent = element.querySelectorAll('.accordion-section-content');
                 const accordionUnderline = element.querySelector('.accordion-section-underline');
